987a6b642559714739ac0da0c2e2468a254219d5
Nate Gay <nate@ohnate.com>
2016-11-24 20:40:57 -0700

    video2

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var HelloWorld = React.createClass({
+	render: function () {
+		return (
+			<div>Hello World!</div>
+		)
+	}
+});
+
+ReactDom.render(
+	<HelloWorld />,
+	document.getElementById('app')
+);
+
