76a0baba82a6df2b38d85a6f6335ee3c3d38aeab
Nate Gay <nate@ohnate.com>
2016-11-29 18:04:08 -0600

    accepts input, more route manipulation

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,20 @@
 var React = require('react');
+var transparentBG = require('../styles').transparentBG;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Home = React.createClass({
 	render: function () {
 		return (
-			<div>Hello from Home!</div>
+			<div className="jumbotron col-sm-12 text-center" style={transparentBG}>
+				<h1>Github Battle</h1>
+				<p className="lead">Some Fancy Moto</p>
+				<Link to="/playerOne">
+					<button type="button" className="btn btn-large btn-success">
+						Get Started
+					</button>
+				</Link>
+			</div>
 		)
 	}
 });
